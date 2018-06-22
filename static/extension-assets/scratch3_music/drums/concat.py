7462c2475ec25f5cf872f34de270be50ad4dfabe
Corey <gnarf37@gmail.com>
2018-06-22 14:33:19 +0000

    Remove stale directories

--- static/extension-assets/scratch3_music/drums/concat.py
+++
@@ -1,8 +0,0 @@
-from pydub import AudioSegment
-
-snare = AudioSegment.from_mp3("1-snare.mp3")
-bass = AudioSegment.from_mp3("2-bass-drum.mp3")
-
-combo = snare + bass
-combo.export("combo.mp3", format="mp3")
-
